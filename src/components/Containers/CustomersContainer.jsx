3b41991433dbf7f2eb61c4b65df71922e19d150f
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-08-30 14:19:39 -0500

    Agregamos navegacion

--- src/components/Containers/CustomersContainer.jsx
+++ src/components/Containers/CustomersContainer.jsx
@@ -3,33 +3,37 @@ import PropTypes from "prop-types";
 import AppFrame from "../AppFrame";
 import CustomersList from "../CustomersList";
 import CustomerActions from "../CustomerActions";
+import { withRouter } from "react-router-dom";
 
 const propTypes = {};
 
 const customers = [
-    {
-        "dni": "27000",
-        "name": "Juan Pereza",
-        "age": 37
-    },
-    {
-        "dni": "30000",
-        "name": "Otro",
-        "age": 23
-    },
-    {
-        "dni": "33000",
-        "name": "Victor Hoyos",
-        "age": 21
-    }
+  {
+    dni: "27000",
+    name: "Juan Pereza",
+    age: 37
+  },
+  {
+    dni: "30000",
+    name: "Otro",
+    age: 23
+  },
+  {
+    dni: "33000",
+    name: "Victor Hoyos",
+    age: 21
+  }
 ];
 
 class CustomersContainer extends React.Component {
+  handleAddNew = () => {
+      this.props.history.push('/customers/new')
+  };
   renderBody = customers => (
     <div>
       <CustomersList
         customers={customers}
-        urlPath={"customer/"}
+        urlPath={"customers/"}
       ></CustomersList>
       <CustomerActions>
         <button onClick={this.handleAddNew}>Nuevo cliente</button>
@@ -42,8 +46,8 @@ class CustomersContainer extends React.Component {
       <div>
         <AppFrame
           header={"Listado de clientes"}
-          body={this.renderBody(customers)}>
-        </AppFrame>
+          body={this.renderBody(customers)}
+        ></AppFrame>
       </div>
     );
   }
@@ -51,4 +55,4 @@ class CustomersContainer extends React.Component {
 
 CustomersContainer.propTypes = propTypes;
 
-export default CustomersContainer;
+export default withRouter(CustomersContainer);
