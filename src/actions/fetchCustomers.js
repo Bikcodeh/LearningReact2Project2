e7124b28b2c83376f1b78cd2f7b377f201701425
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-02 10:21:54 -0500

    Refactorizacion de hacer la peticion al json

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,11 +1,11 @@
 import { FETCH_CUSTOMERS } from "../constants";
 import { createAction } from "redux-actions";
+import { apiGet } from './../api/index';
+import { urlCustomers } from './../api/urls';
 
-const url = 'http://localhost:3001/customers'
 
-const apiFetchCustomers = () => fetch(url).then(r => r.json());
 
 //cuando el middleware redux promise detecta que como payload estamos pasando una promise
 //entonces ejecuta la promise y una vez que obtiene el resultado genera una accion que ahi si se termina de resolver
 //y la toma el reducer
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiGet(urlCustomers));
