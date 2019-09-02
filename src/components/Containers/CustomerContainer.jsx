f033766d05666cd6dd6a51629a1027db20e8f4e3
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-02 15:58:12 -0500

    Utilizacion de Route en CustomerContainer y su parametro children

--- src/components/Containers/CustomerContainer.jsx
+++ src/components/Containers/CustomerContainer.jsx
@@ -3,15 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import AppFrame from './../AppFrame';
 import { getCustomerByDni } from './../../selectors/customers';
+import { Route } from 'react-router-dom';
 
 class CustomerCotainer extends React.Component {
-
+    
+    renderBody = () => {
+        
+        return <Route path='/costumers/:dni/edit' children={ 
+            (props) => (props.location.pathname.includes('edit') ? <p>Esto es la edicion</p> : <p>No es edicion</p>)
+        } />
+    }
+    //<p>Datos del cliente: "{this.props.customer.name}"</p>
     render() {
         return (
             <div>
                 <AppFrame 
                     header={`Cliente ${this.props.dni}`} 
-                    body={<p>Datos del cliente: "{this.props.customer.name}"</p>}>
+                    body={this.renderBody()}>
                 </AppFrame>
             </div>
         )
