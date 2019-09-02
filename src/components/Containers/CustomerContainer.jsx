4a652e28a6ce7db2111921ff2fc95d8cdcc60184
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-02 16:22:23 -0500

    Refactor para determinar cual componente mostrar

--- src/components/Containers/CustomerContainer.jsx
+++ src/components/Containers/CustomerContainer.jsx
@@ -4,13 +4,18 @@ import PropTypes from 'prop-types'
 import AppFrame from './../AppFrame';
 import { getCustomerByDni } from './../../selectors/customers';
 import { Route } from 'react-router-dom';
+import CustomerEdit from './../CustomerEdit';
+import CustomerData from './../CustomerData';
 
 class CustomerCotainer extends React.Component {
     
     renderBody = () => {
         
         return <Route path='/costumers/:dni/edit' children={ 
-            (props) => (props.location.pathname.includes('edit') ? <p>Esto es la edicion</p> : <p>No es edicion</p>)
+            (props) => {
+                const CustomerControl = props.location.pathname.includes('edit') ? CustomerEdit : CustomerData;
+                return <CustomerControl {...this.props.customer} />
+            }
         } />
     }
     //<p>Datos del cliente: "{this.props.customer.name}"</p>
