f0797e83b84d0be561146ff0f2b82aa374961c04
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-02 14:50:17 -0500

    Refactor de obtener los datos del customer e implementacion de reselect

--- src/selectors/customers.js
+++ src/selectors/customers.js
@@ -1,1 +1,6 @@
+import { createSelector } from "reselect";
+
+export const getCustomerByDni = createSelector((state, props) => state.customers.find(c => c.dni === props.dni), customer => customer);
+
 export const getCustomers = state => state.customers;
+
