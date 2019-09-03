e4847bf1e9b1dc7b70969786517cdbbf0cd07e3e
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-03 09:53:46 -0500

    Multiples validaciones a nivel field y refactor

--- src/components/CustomerEdit.jsx
+++ src/components/CustomerEdit.jsx
@@ -1,66 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { reduxForm, Field } from 'redux-form';
-import { connect } from 'react-redux';
-import { setPropsAsInitial } from './../helpers/setPropsAsInitial';
+import React from "react";
+import PropTypes from "prop-types";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { setPropsAsInitial } from "./../helpers/setPropsAsInitial";
 
 const propTypes = {
-    name: PropTypes.string,
-    dni: PropTypes.string,
-    age: PropTypes.number,
-}
+  name: PropTypes.string,
+  dni: PropTypes.string,
+  age: PropTypes.number
+};
 
-const isRequired = value => (
-    !value && "Este campo es requerido"
+const isRequired = value => !value && "Este campo es requerido";
+
+const isNumber = value => isNaN(Number(value)) && "El campo debe ser un numero";
+
+const MyField = ({ input, meta, type, label }) => (
+  <div>
+    <label htmlFor={label}>{label}</label>
+    <input {...input} type={!type ? "text" : type} />
+    {meta.touched && meta.error && <span>{meta.error}</span>}
+  </div>
 );
 
-const MyField = ({input, meta}) => (
+const CustomerEdit = ({ name, dni, age }) => {
+  return (
     <div>
-        <input {...input} type="text"/>
-        {
-            meta.touched && meta.error && <span>{meta.error}</span>
-        }
+      <h2>Edicion del cliente</h2>
+      <form action="">
+        <Field
+          name="name"
+          component={MyField}
+          type="text"
+          validate={isRequired}
+          label="Nombre"
+        ></Field>
+
+        <Field
+          name="dni"
+          component={MyField}
+          type="text"
+          validate={[isRequired, isNumber]}
+          label="Dni"
+        ></Field>
+
+        <Field
+          name="age"
+          component={MyField}
+          type="number"
+          validate={isNumber}
+          label="Edad"
+        ></Field>
+      </form>
     </div>
-);
+  );
+};
 
-const CustomerEdit = ({ name, dni, age}) => {
-    return (
-        <div>
-            <h2>Edicion del cliente</h2>
-           <form action="">
-               <div>
-                   <label htmlFor="name">Nombre</label>
-                   <Field 
-                        name="name" 
-                        component={MyField} 
-                        type="text"
-                        validate={isRequired}>
-                    </Field>
-               </div>
-               <div>
-                   <label htmlFor="dni">DNI</label>
-                   <Field 
-                        name="dni" 
-                        component={MyField} 
-                        type="text"
-                        validate={isRequired}>
-                    </Field>
-               </div>
-               <div>
-                   <label htmlFor="age">Edad</label>
-                   <Field 
-                        name="age" 
-                        component="input" 
-                        type="number">
-                    </Field>
-               </div>
-           </form>
-        </div>
-    )
-}
+CustomerEdit.propTypes = propTypes;
 
-CustomerEdit.propTypes = propTypes
-
-const CustomerEditForm  = reduxForm({ form: 'CustomerEdit'})(CustomerEdit)
+const CustomerEditForm = reduxForm({ form: "CustomerEdit" })(CustomerEdit);
 
 export default setPropsAsInitial(CustomerEditForm);
