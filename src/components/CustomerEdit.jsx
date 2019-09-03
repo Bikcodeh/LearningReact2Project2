27ab57a8311849ef0be89ce8cead13a9e7f8e99c
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-03 08:00:04 -0500

    Creacion HOC, setPropsAsInitial

--- src/components/CustomerEdit.jsx
+++ src/components/CustomerEdit.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
+import { setPropsAsInitial } from './../helpers/setPropsAsInitial';
 
 const propTypes = {
     name: PropTypes.string,
@@ -33,10 +34,6 @@ const CustomerEdit = ({ name, dni, age}) => {
 
 CustomerEdit.propTypes = propTypes
 
-const mapStateToProps = (state, props) => ({
-    initialValues: props
-})
-
 const CustomerEditForm  = reduxForm({ form: 'CustomerEdit'})(CustomerEdit)
 
-export default connect(mapStateToProps)(CustomerEditForm);
+export default setPropsAsInitial(CustomerEditForm);
