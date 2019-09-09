11dc218a0bb91f63ce8b9248fb8e73294b36ab3b
Victor.Micalisoft <victor.micalisoft@gmail.com>
2019-09-09 13:36:21 -0500

    Usando funcion reduce

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,9 +1,30 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS, INSERT_CUSTOMER } from './../constants/index';
+import { FETCH_CUSTOMERS, INSERT_CUSTOMER, UPDATE_CUSTOMER } from './../constants/index';
 
 //El primer argumento del handleActions son las acciones y el segundo es un estado inicial
 export const customers = handleActions({
     [FETCH_CUSTOMERS]: (state, action) => [ ...action.payload],
     //En el payload del action va a estar la promise
-    [INSERT_CUSTOMER]: (state, action) => [ ...state, action.payload]
+    [INSERT_CUSTOMER]: (state, action) => [ ...state, action.payload],
+    [UPDATE_CUSTOMER]: (state, action) => {
+        const customerPayload = action.payload;
+        const { id } = customerPayload;
+
+        // [ { id: 1, name: '', ...},
+        //   { id: 2, name: '', ...},
+        //   { id: 3, name: '', ...}
+        // ] 
+        const customers = state;
+        const initialValue = []
+        // [ ]
+        const newCustomers = customers.reduce((acc, customer) => {
+            if(customer.id === id){
+                return [ ...acc, customerPayload];
+            }else{
+                return [ ...acc, customer]
+            }
+        }, initialValue);
+
+        return newCustomers;
+    }
 }, []);
